193bd87d090517940501ff2a4e5f60108ac1426a
wangsanity <wangqingshui1985@163.com>
2025-01-24 09:09:35 +0000

    Update Input.tsx

--- pc-web-components/src/input/Input.tsx
+++ pc-web-components/src/input/Input.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-export type InputProps = React.HTMLAttributes<HTMLInputElement>;
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
+  errorMessage?: string;
+  showError?: boolean;
+  errorTooltip?: string;
+}
 
 // eslint-disable-next-line
 export const Input = ({ style, ...inputProps }: InputProps) => {
   const defaultStyle: React.CSSProperties = {
     borderRadius: '4px',
-    padding: '6px 8px',
+    padding: '10px 12px',
+    minWidth: '200px',
     border: 'solid 1px #ddd'
   };
 
